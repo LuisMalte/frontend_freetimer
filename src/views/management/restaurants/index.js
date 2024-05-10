103f12d17196c0fe91e740ef87df147b591a9d54
LuisMalte <lcmalte99839@umanizales.edu.co>
2024-05-10 15:33:40 -0500

    primero luis

---
+++ src/views/management/restaurants/index.js
@@ -0,0 +1,8 @@
+import Restaurant from "./restaurant/Restaurant";
+import RestaurantForm from "./restaurant/RestaurantForm";
+import RestaurantEditForm from "./restaurant/RestaurantEditForm";
+export {
+    Restaurant,
+    RestaurantForm,
+    RestaurantEditForm  
+}
